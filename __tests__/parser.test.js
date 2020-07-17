f9a377845659fce09d7c66b11b945ed570d0ac12
trovati <watsontrovati@gmail.com>
2020-07-17 15:07:53 -0300

    Terminando os exemplos de teste TDD

--- __tests__/parser.test.js
+++ __tests__/parser.test.js
@@ -1,15 +1,37 @@
 const parser = require("../parser");
 const fs = require("fs");
+const { hasUncaughtExceptionCaptureCallback } = require("process");
 let html;
+let listings;
 
 
 beforeAll(() => {
     html = fs.readFileSync("./test.html");
+    listings = parser.listings(html);
 });
 
 
 
-it("shold give 4", () => {
-    const result = parser.add(2,2);
-    expect(result).toBe(4);
+it("should give the correct number of listings", () => {
+    expect(listings.length).toBe(120);
 });
+
+it("should get hood from listings", () => {
+    expect(listings[0].hood).toBe("");
+});
+
+it("should get correct date from listings", () => {
+    expect(listings[0].datePosted).toStrictEqual(new Date("2020-07-16T20:28:00.000Z"));
+});
+
+it("should get correct url", () => {
+    expect(listings[0].url).toBe("https://sfbay.craigslist.org/eby/muc/d/san-ramon-looking-to-form-power-metal/7160687600.html");
+});
+
+it("should get correct title", () => {
+    expect(listings[0].title).toBe("Looking to form power metal band in East bay.");
+});
+
+
+
+
--- parser.js
+++ parser.js
@@ -1,1 +1,28 @@
-exports.add = (number1, number2) => number1 + number2;
+const cheerio = require("cheerio");
+
+function getDatePosted($, element) {
+    return new Date(
+        $(element)
+        .find(".result-date")
+        .attr("datetime")
+    );
+}
+
+function getHood($, element) {
+    return $(element)
+    .find("result-hood")
+    .text();
+}
+
+exports.listings = (html) => {
+    const $ = cheerio.load(html);
+    return $(".result-info")
+    .map((index, element) => {
+        const titleElement = $(element).find(".result-title.hdrlnk")
+        const title = titleElement.text();
+        const url = titleElement.attr("href");
+        const hood = getHood($, element);
+        const datePosted = getDatePosted($, element);
+        return { title, url, hood, datePosted };
+    }).get()
+}
